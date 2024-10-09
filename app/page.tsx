d1ef17a72d693a4eaa7e34a1187f6366fbbea64b
Alejandro Israel Gonzalez <imthebest_1997@hotmail.com>
2024-10-09 10:32:47 -0500

    Links

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 font-[family-name:var(--font-geist-sans)]">
       <span className="text-5xl">Hola Tiux !!</span>
+
+      <Link href={"/about"}> About Page </Link>
     </main>
   );
 }
--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,4 @@
 export * from "./navbar/Navbar";
 
 // Client components
-export * from "./active-link/ActiveLink";
+export { ActiveLink } from "./active-link/ActiveLink";
